1076754d4410ac7982a3e6497e4d7eaf18a6d815
gunner73 <amit.gunner73@gmail.com>
2020-04-19 16:18:11 +0600

    Add files via upload

---
+++ Area of a Circle.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    const double pie=3.14159;
+    double a,r ;
+
+    cin>> r ;
+
+    cout<< "A=" << fixed << setprecision(4) << pie*r*r << endl ;
+
+    return 0 ;
+}
